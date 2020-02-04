2163291f532754bcfc73e09e736e8a6cfd15d775
Kamal Norizan <kamalnorizan@gmail.com>
2020-02-04 15:34:06 +0800

    carbonblade

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -3,13 +3,24 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{$post->title}}</div>
 
                 <div class="card-body">
                     <p>Author: {{$post->user->name}}</p>
                    <p>{{$post->content}}</p>
+                </div>
+            </div>
+            <br>
+            <div class="card">
+                <div class="card-header">Comments</div>
+
+                <div class="card-body">
+                    @foreach ($post->comments as $comment)
+                    <p>{{$comment->user->name}}<br> <small>{{\Carbon\Carbon::parse($comment->created_at)->format('d-m-Y')}}</small></p>
+
+                    @endforeach
                 </div>
             </div>
         </div>
