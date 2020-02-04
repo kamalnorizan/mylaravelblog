b420375e7f247b6b5398f86b9afde28b893ab6ce
Kamal Norizan <kamalnorizan@gmail.com>
2020-02-04 12:36:39 +0800

    comment model

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -8,5 +8,8 @@ use Faker\Generator as Faker;
 $factory->define(Comment::class, function (Faker $faker) {
     return [
         //
+        'comment' => $faker->sentence(rand(5,15)),
+        'user_id' => rand(1,51),
+        'post_id' => rand(1,300),
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         // factory(App\User::class,50)->create();
-        factory(App\Post::class,300)->create();
+        factory(App\Comment::class,600)->create();
     }
 }
