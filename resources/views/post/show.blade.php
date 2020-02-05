a42839199ed264393d4a689c2836e75f18c09742
Kamal Norizan <kamalnorizan@gmail.com>
2020-02-05 12:38:04 +0800

    before rehat

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,33 +1,50 @@
 @extends('layouts.app')
 
+@section('head')
+
+@endsection
+
+@section('maintitle','Post Details')
+
+@section('actionbutton')
+
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">{{$post->title}}</div>
-
-                <div class="card-body">
-                    <p>Author: {{$post->user->name}}</p>
-                    <p>{{$post->content}}</p>
-                </div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="ibox float-e-margins">
+            <div class="ibox-title">
+                <h5>{{$post->title}}</h5>
             </div>
-            <br>
-            <div class="card">
-                <div class="card-header">Comments</div>
-
-                <div class="card-body">
-                    @foreach ($post->comments as $comment)
-                    <p>{{$comment->user->name}}<br>
-                        <small>{{\Carbon\Carbon::parse($comment->created_at)->format('d-m-Y')}}</small>
-                        <br>
-                        {{$comment->comment}}
-                    </p>
-                    <hr>
-                    @endforeach
-                </div>
+            <div class="ibox-content">
+                <p>Author: {{$post->user->name}}</p>
+                <p>{{$post->content}}</p>
             </div>
         </div>
     </div>
 </div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="ibox float-e-margins">
+            <div class="ibox-title">
+                <h5>Comments</h5>
+            </div>
+            <div class="ibox-content">
+                @foreach ($post->comments as $comment)
+                <p>{{$comment->user->name}}<br>
+                    <small>{{\Carbon\Carbon::parse($comment->created_at)->format('d-m-Y')}}</small>
+                    <br>
+                    {{$comment->comment}}
+                </p>
+                <hr>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('script')
+
 @endsection
