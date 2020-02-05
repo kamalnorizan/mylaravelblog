a4b1ef77b94c160973767fd185840ba4e65cb788
Kamal Norizan <kamalnorizan@gmail.com>
2020-02-05 12:15:13 +0800

    post blade

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,58 +1,63 @@
 @extends('layouts.app')
 
-@section('maintitle')
-This is maintitle
+@section('head')
+
+@endsection
+
+@section('maintitle','Posts')
+
+@section('actionbutton')
+
 @endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">Posts</div>
-
-                <div class="card-body">
-                    <table class="table table-hover">
-                        <tr>
-                            <td>
-                                #
-                            </td>
-                            <td>
-                                Title
-                            </td>
-                            <td>
-                                Publish Date
-                            </td>
-                            <td>
-                                Author
-                            </td>
-                            <td>
-                                Action(s)
-                            </td>
-                        </tr>
-                        @foreach ($posts as $key=>$post)
-                        <tr>
-                            <td>
-                                {{$key+1+(10*($posts->currentPage()-1))}}
-                            </td>
-                            <td>
-                                {{$post->title}}
-                            </td>
-                            <td>
-                                {{$post->publish_at}}
-                            </td>
-                            <td>
-                                {{$post->user->name}}
-                            </td>
-                            <td>
-                                <a href="/post/{{$post->id}}" class="btn btn-primary btn-sm">Show</a>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </table>
-                    <div class="d-flex justify-content-center">
-                        {{$posts->links()}}
-                    </div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="ibox float-e-margins">
+            <div class="ibox-title">
+                <h5>Post List</h5>
+            </div>
+            <div class="ibox-content">
+                <table class="table table-hover">
+                    <tr>
+                        <td>
+                            #
+                        </td>
+                        <td>
+                            Title
+                        </td>
+                        <td>
+                            Publish Date
+                        </td>
+                        <td>
+                            Author
+                        </td>
+                        <td>
+                            Action(s)
+                        </td>
+                    </tr>
+                    @foreach ($posts as $key=>$post)
+                    <tr>
+                        <td>
+                            {{$key+1+(10*($posts->currentPage()-1))}}
+                        </td>
+                        <td>
+                            {{$post->title}}
+                        </td>
+                        <td>
+                            {{$post->publish_at}}
+                        </td>
+                        <td>
+                            {{$post->user->name}}
+                        </td>
+                        <td>
+                            <a href="/post/{{$post->id}}" class="btn btn-primary btn-sm">Show</a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </table>
+                <div class="d-flex justify-content-center">
+                    {{$posts->links()}}
                 </div>
             </div>
         </div>
@@ -60,5 +65,6 @@ This is maintitle
 </div>
 @endsection
 
+@section('script')
 
-
+@endsection
